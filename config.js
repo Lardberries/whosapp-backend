8c911322f2e914f039ecf69bd3f9163fc01ee0a5
Matt Howard <Matt Howard>
2016-07-19 20:16:09 -0400

    Forgot comma

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     'secret': '',
     'database': '',
     'emojiTimeout': 30 * 60 * 1000, // 30 minutes
-    'maxMessageLength': 2000 // max number of characters per message
+    'maxMessageLength': 2000, // max number of characters per message
     'secret': 'mynameaborat',
     'database': 'mongodb://localhost/whosapp'
 };
