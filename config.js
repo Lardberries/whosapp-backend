987d077e015b3dc34086214bc9889f4fd8abae41
Jacob Sanchez <jacob.sanchez@huskers.unl.edu>
2016-07-21 13:53:51 -0400

    refresh every 2 minutes

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'emojiTimeout': 30 * 60 * 1000, // 30 minutes
+    'emojiTimeout': 2 * 60 * 1000, // 30 minutes
     'maxMessageLength': 2000, // max number of characters per message
     'secret': 'mynameaborat',
     'database': 'mongodb://localhost/whosapp',
