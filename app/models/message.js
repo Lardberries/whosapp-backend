4cf589e214b05f948bc2bab7115cb71596c1586e
Jacob Sanchez <jacob.sanchez@huskers.unl.edu>
2016-07-21 09:21:35 -0400

    message colors

--- app/models/message.js
+++ app/models/message.js
@@ -1,15 +1,20 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var emojiUtil = require('../utils/emoji');
 
 var ObjectId = mongoose.Schema.Types.ObjectId;
 
 var schema = new Schema({
     sender: {type: ObjectId, required: true}, // User
     chatid: {type: ObjectId, required: true}, // Chat
-    emoji: {type: String, required: true}, // Sender
+    emoji: {type: String, required: true},
     time: {type: Date, required: true},
     seq: {type: Number, required: true}, // Sequence number
     content: {type: String, required: true}
+});
+
+schema.virtual('color').get(function() {
+	return emojiUtil.getColorForEmoji(this.emoji);
 });
 
 var Message = module.exports = mongoose.model('Message', schema);
--- app/utils/emoji.js
+++ app/utils/emoji.js
@@ -1,6 +1,10 @@
+var _ = require('underscore');
 var emoji = module.exports = {};
 
 var whosappEmojis = emoji.whosappEmojis = '😊,😁,😂,😇,😎,😆,👽,😈,👻,💪,👯,👮,👼,👶,🎈,🎂,💚,🎷,🎩,⚽,🎱,🎓,🏈,☎,📍,🔪,🍜,🍍,🌽,🍨,🍀,🌵,🐉,🐎,🐋,🐕,🐥,🙉,☃,🐼'.split(',');
+var whosappColors = emoji.whosappColors = '#99C764,#D66D6D,#6DABD6,#6DD6A2,#ECEF89,#D483EC,#FDA267,#C49B75,#8972B5,#EB62DD,#44B764,#F7CAC9,#F7786B,#91A8D0,#034F84,#FAE03C,#98DDDE,#9896A4,#DD4132,#B18F6A,#79C753,#9DC3D4,#00ABC0,#7ACCB8,#D2B49C,#E78B90,#F88F58,#E5D68E,#964F4C,#C5C6C7,#7BA0C0,#476A30,#B18EAA,#9FD66D,#F86043,#6D8ED6,#7821BA,#50E3C2,#BD0FE1,#B8E986'.split(',');
+
+var emojiColorMap = _.object(whosappEmojis, whosappColors);
 
 /* Generate a randomly ordered sequence of the WhosApp emojis
  * Returns - an array of emojis
@@ -16,4 +20,8 @@ emoji.generateSequence = function generateSequence() {
   }
 
   return output.join(',');
+}
+
+emoji.getColorForEmoji = function getColorForEmoji(emoji) {
+	return emojiColorMap[emoji];
 }
